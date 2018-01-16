4b21785df11ef2dc49fdfedff847749534828eab
xuyunxiang <xuyunxiang@jiashuangkuanzi.com>
2018-01-16 18:41:03 +0800

    构造运行文件

--- bin/jn-wxxcx-cli.js
+++ bin/jn-wxxcx-cli.js
@@ -1,2 +1,28 @@
 #!/usr/bin/env node
 
+var ejs = require('ejs')
+var fs = require('fs')
+var mkdirp = require('mkdirp')
+var path = require('path')
+var program = require('commander')
+var readline = require('readline')
+var sortedObject = require('sorted-object')
+var util = require('util')
+
+var MODE_0666 = parseInt('0666', 8)
+var MODE_0755 = parseInt('0755', 8)
+
+/**
+ * Mkdir -p.
+ *
+ * @param {String} path 文件夹路径
+ * @param {Function} fn  回调函数
+ */
+
+function mkdir (path, fn) {
+  mkdirp(path, MODE_0755, function (err) {
+    if (err) throw err
+    console.log('   \x1b[36mcreate\x1b[0m : ' + path)
+    fn && fn()
+  })
+}
